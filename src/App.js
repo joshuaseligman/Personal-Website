942e477c2d76cd19641a6a3cad1765c044f4cb02
joshuaseligman <joshua.seligman1@marist.edu>
2021-03-03 23:21:25 -0500

    Updated website settings

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App () {
 
   return (
     <div className='App'>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename='/'>
         <Header />
         <Switch>
           <Route path='/' exact component={() => <Home />}/>
